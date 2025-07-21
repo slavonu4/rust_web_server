542269d8e9135717acc0cc1e8d22f368f0a8d7f4
slavonu4 <slavonu4@gmail.com>
2025-07-21 12:13:49 +0300

    Add query params to requests

--- src/http/request/mod.rs
+++ src/http/request/mod.rs
@@ -14,12 +14,14 @@ pub struct Request {
     url: String,
     method: RequestMethod,
     headers: HashMap<String, Vec<String>>,
+    query_params: HashMap<String, Vec<String>>,
 }
 
 pub struct RequestBuilder {
     url: String,
     method: RequestMethod,
     headers: HashMap<String, Vec<String>>,
+    query_params: HashMap<String, Vec<String>>,
 }
 
 impl Request {
@@ -28,6 +30,7 @@ impl Request {
             url: builder.url,
             method: builder.method,
             headers: builder.headers,
+            query_params: builder.query_params,
         }
     }
 
@@ -36,11 +39,16 @@ impl Request {
             url: String::from(""),
             method: RequestMethod::GET,
             headers: HashMap::new(),
+            query_params: HashMap::new(),
         }
     }
 
     pub fn get_header(&self, header_name: &str) -> Option<&Vec<String>> {
         self.headers.get(header_name)
+    }
+
+    pub fn get_query_param(&self, query_param_name: &str) -> Option<&Vec<String>> {
+        self.query_params.get(query_param_name)
     }
 }
 
@@ -70,6 +78,21 @@ impl RequestBuilder {
         self
     }
 
+    pub fn add_query_param(
+        mut self,
+        query_param_name: impl Into<String>,
+        query_param_value: impl Into<String>,
+    ) -> Self {
+        let query_param_value = Into::into(query_param_value);
+
+        self.query_params
+            .entry(Into::into(query_param_name))
+            .and_modify(|v| v.push(query_param_value.clone()))
+            .or_insert(vec![query_param_value.clone()]);
+
+        self
+    }
+
     pub fn build(self) -> Request {
         Request::new(self)
     }
@@ -88,6 +111,9 @@ mod tests {
             .add_header("test_header_1", "test_value_1_1")
             .add_header("test_header_1", "test_value_1_2")
             .add_header("test_header_2", "test_value_2_1")
+            .add_query_param("test_qp_1", "test_qp_value_1_1")
+            .add_query_param("test_qp_1", "test_qp_value_1_2")
+            .add_query_param("test_qp_2", "test_qp_value_2_1")
             .build();
 
         assert_eq!(
@@ -113,6 +139,26 @@ mod tests {
         assert!(
             header_3.is_none(),
             "Request must not have header 'test_header_3'"
+        );
+
+        let query_param_1 = request.get_query_param("test_qp_1");
+        println!("{}", query_param_1.is_some());
+        assert!(
+            query_param_1.is_some_and(|qp| qp.len() == 2 && qp.contains(&String::from("test_qp_value_1_1")) && qp.contains(&String::from("test_qp_value_1_2"))),
+            "Query param 'test_qp_1' must be present and contain 2 values('test_qp_value_1_1' and 'test_qp_value_1_2')"
+        );
+
+        let query_param_2 = request.get_query_param("test_qp_2");
+        assert!(
+            query_param_2
+                .is_some_and(|qp| qp.len() == 1 && qp.contains(&String::from("test_qp_value_2_1"))),
+            "Query param 'test_qp_2' must be present and contain only 1 value('test_qp_value_2_1')"
+        );
+
+        let query_param_3 = request.get_query_param("test_qp_3");
+        assert!(
+            query_param_3.is_none(),
+            "Request must not have header 'test_qp_3'"
         );
     }
 }
